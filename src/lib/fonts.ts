e34682f5b45ee18e5604fa0447985d652706389a
Aidan Wickes <aidan.wickes@gmail.com>
2024-09-10 16:44:14 +0100

    empty project

--- src/lib/fonts.ts
+++
@@ -1,8 +0,0 @@
-import { Zilla_Slab as zillaSlab } from 'next/font/google'
-
-export const zilla = zillaSlab({
-    subsets: ['latin'],
-    display: 'swap',
-    variable: '--font-zilla',
-    weight: '400',
-})
